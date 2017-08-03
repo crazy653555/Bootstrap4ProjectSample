33b78a734aa8d064fd946cf118bd71829e1a8244
eason <crazy653555@gmail.com>
2017-08-03 17:04:39 +0800

    使用蜘蛛圖，使用 Highcharts 外掛

--- js/dashboard.js
+++ js/dashboard.js
@@ -9,4 +9,54 @@ $(document).ready(function(){
     'trackColor':'#e5e9ec',
     'size':190
   });
+
+
+  $('#performance-eval .spider-chart').highcharts({
+    chart:{
+      polar:true,
+      type:'area'
+    },
+
+    title:{
+      text:''
+    },
+
+    xAxis:{
+      categories:['Taming','Acessory','Development','Grooming','Awareness','Ration'],
+      tickmarkPlacement:'on',
+      lineWidth:0
+    },
+
+    yAxis:{
+      gridLineInterpolation:'polygon',
+      lineWidth:0,
+      min:0
+    },
+
+    tooltip:{
+      shared:true,
+      pointFormat:'<span style="color:{series.color}">{series.name}:<b>${point.y:,.Of}</b><br/>'
+    },
+
+    legend:{
+      align:'right',
+      verticalAlign:'top',
+      y:70,
+      layout:'vertical'
+    },
+
+    series:[{
+      name:'Allocated resources',
+      data:[45000,39000,58000,63000,38000,93000],
+      pointPlacement:'on',
+      color:'#676F84'
+    },
+    {
+      name:'Spent resources',
+      data:[83000,49000,60000,35000,77000,90000],
+      pointPlacement:'on',
+      color:'#f35958' 
+    }]
+  });
+
 });
