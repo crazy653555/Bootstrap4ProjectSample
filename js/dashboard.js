d38b300916152a415e74e8148513a41cc714f1ac
eason <crazy653555@gmail.com>
2017-08-03 16:12:56 +0800

    建立圓形圖表，使用 Easy Pie Chart 外掛

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,3 +1,12 @@
 $(document).ready(function(){
-  $('[data-toggle]="tooltip').tooltip();
-})
+  $('[data-toggle="tooltip"]').tooltip();
+
+  $('.round-chart').easyPieChart({
+    'scaleColor':false,
+    'lineWidth':20,
+    'lineCap':'butt',
+    'barColor':'#6d5cae',
+    'trackColor':'#e5e9ec',
+    'size':190
+  });
+});
