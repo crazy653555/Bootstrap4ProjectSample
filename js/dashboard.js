01e7608f3f08aa9628433279a658e572327c3d42
eason <crazy653555@gmail.com>
2017-08-04 19:05:02 +0800

    用 bootstrap 元件再做出另一張卡片

--- js/dashboard.js
+++ js/dashboard.js
@@ -137,5 +137,19 @@ $(document).ready(function () {
   });
 
 
+  changeMultiplier=0.2;
+  window.setInterval(function(){
+    var freeSpacePercentage;
+
+    freeSpacePercentage = $('#free-space').text();
+    freeSpacePercentage = parseFloat(freeSpacePercentage);
+
+    delta = changeMultiplier * (Math.random() < 0.5 ? -1.0 : 1.0 );
+
+    freeSpacePercentage = freeSpacePercentage + freeSpacePercentage * delta;
+    freeSpacePercentage = parseInt(freeSpacePercentage);
+
+    $('#free-space').text(freeSpacePercentage + '%');
+  },2000);
 
 });
